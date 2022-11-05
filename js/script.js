04c13acabcb0ab789145120cf106c02a0162feff
Mosa-code <112524403+Mosa-code@users.noreply.github.com>
2022-11-05 14:30:09 -0500

    fixed centered navigation

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function init(){
+  const textbox = document.getElementById("entryinput")
+  const button = document.getElementById("entrybutton")
+
+  
+  
+  
+  
+
+}
+window.addEventListener('load', init);
